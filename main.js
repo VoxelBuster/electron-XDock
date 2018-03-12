f8649b2217d376d44c19faabd40b5d03e6c09871
Galen Nare <voxelbustergaming@gmail.com>
2018-03-12 19:46:58 -0400

    electron caught the gay

--- main.js
+++ main.js
@@ -1,45 +1,41 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
-
+const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let win
 
 function createWindow () {
   // Create the browser window.
-  setTimeout(function() {
-    mainWindow = new BrowserWindow({
-      width: 1920,
-      height: 1080,
-      transparent: true,
-      frame: false,
-      toolbar: false,
-      icon: "assets/ico/appicon.png"
-    })
+  win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    transparent: false,
+    frame: true,
+    toolbar: false,
+    icon: "assets/ico/appicon.png",
+    webPreferences:  {
+      webSecurity: false
+    }})
 
-    mainWindow.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }))
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+  // Open the DevTools.
+  win.webContents.openDevTools()
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
-      mainWindow = null
-    })
-  }, 40)
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
 }
 
 // This method will be called when Electron has finished
@@ -48,21 +44,18 @@ function createWindow () {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if (win === null) {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
