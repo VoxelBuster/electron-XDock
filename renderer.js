40216efe2e93d4d6c9917b69220a8b74cce17521
Galen Nare <voxelbustergaming@gmail.com>
2018-03-12 20:32:10 -0400

    ayy now we're getting somewhere

--- renderer.js
+++ renderer.js
@@ -1,12 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var imageMap = {
-  centerCicle: document.getElementById('centerCicle')
-};
+var imageMap;
 
 const protonAPI = require('./protonAPI.js');
-const appSettings = require('./appSettings.js');
+// const appSettings = require('./appSettings.js');
 const geom = require( './geometry.js');
 const $ = require('jQuery');
 
@@ -17,8 +15,18 @@ $(document).ready(function() {
   ctx = dockCanvas.getContext('2d');
   cvWidth = dockCanvas.width;
   cvHeight = dockCanvas.height;
+  preprocessImages();
   animate();
-})
+});
+
+function preprocessImages() {
+  imageMap = {
+    centerCircle: document.getElementById('centerCircle')
+  };
+
+  imageMap.centerCircle.width *= screenRatio;
+  imageMap.centerCircle.height *= screenRatio;
+}
 
 function animate() {
   // call again next time we can draw
@@ -26,13 +34,12 @@ function animate() {
   // clear canvas
   ctx.clearRect(0, 0, cvWidth, cvHeight);
   drawCenterCircle();
+
   ctx.fillStyle = '#ff0000';
-  
   protonAPI.sleep(16); // Limit to 60fps
 }
 
 function drawCenterCircle() {
-  var centerCirclePt = protonAPI.centerObject(new geom.Rectangle(imageMap.centerCicle.width, imageMap.centerCicle.height), new geom.Rectangle(cvWidth, cvHeight));
-  ctx.drawImage(imageMap.centerCicle, centerCirclePt.x, centerCirclePt.y,
-    imageMap.centerCicle.width * appSettings.screenRatio, imageMap.centerCicle.height * appSettings.screenRatio);
+  var centerCirclePt = protonAPI.centerObject(new geom.Rectangle(0,0,imageMap.centerCircle.width, imageMap.centerCircle.height), new geom.Rectangle(0,0,cvWidth, cvHeight));
+  ctx.drawImage(imageMap.centerCircle, centerCirclePt.x, centerCirclePt.y, imageMap.centerCircle.width, imageMap.centerCircle.height);
 }
