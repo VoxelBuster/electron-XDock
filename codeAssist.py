f337c64855dd418a34227da3c9a96278bde38480
Galen Nare <voxelbustergaming@gmail.com>
2018-05-09 20:33:22 -0400

    not sure what broke

--- codeAssist.py
+++ codeAssist.py
@@ -4,12 +4,23 @@ import sys
 import re
 
 print 'WARNING: This script may crash on excessively large files as it reads the entire file into memory!'
-if sys.argv[0] == 'dictify':
-    fname = sys.argv[1]
-    f = open(fname, 'rw')
+if sys.argv[1] == 'dictify':
+    print 'Moving global attributes into a dictionary...'
+    fname = sys.argv[2]
+    f = open(fname, 'r+')
     sbuf = f.read()
-    f.close()
     strarr = sbuf.split('\n')
-    matches = []
-    for string in strarr:
-        matches.append(re.match('\w+\s=\s.+', string))
+    kvList = []
+    for x in range(len(strarr)):
+        match = re.match('^\w+\s=\s.+', strarr[x])
+        if match is not None:
+            matchStr = match.string
+            kvList.append((x, 'fields[\'' + re.compile('\s=\s').split(matchStr)[0] + '\'] = ' + re.compile('\s=\s').split(matchStr)[1]))
+    strarr.insert(0, 'fields = {}\n')
+    for item in kvList:
+        strarr[item[0]] = item[1]
+    f.truncate(0)
+    for line in strarr:
+        f.write(line + '\n')
+    f.close()
+    print 'Operation completed'
